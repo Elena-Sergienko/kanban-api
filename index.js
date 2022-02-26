7814abc75fa75066b4f45623371c0886efa9afa0
Elena Sergienko <elenasergienko@Elenas-MacBook-Pro-2.local>
2022-02-25 17:24:29 -0800

    upd structure

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const routes = require("./routes");
+const bodyParser = require("./bodyParser");
 const app = express();
 const PORT = process.env.PORT || 3000; // https://kanban-first.herokuapp.com/
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
 
-let cards = [
-    {id: '1', name: "First card", status: "todo", priority: 2},
-    {id: '2', name: "Second card", status: "done", priority: 1},
-    {id: '3', name: "Third card", status: "in progress", priority: 3},
-    {id: '4', name: "Fourth card", status: "todo", priority: 2},
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
-
-app.get('/cards', (req, res) => {
-    res.send(cards);
-})
-
-app.delete('/card/:cardId', (req, res) => {
-    const cardId = req.params.cardId;
-    cards = cards.filter(el => el.id !== cardId);
-    res.send(cards)
-})
-
-app.post('/card', (req, res) => {
-    const card = req.body;
-    cards.push({id: Math.random().toString(), ...card});
-    res.send('Card created!');
-})
-
-app.patch('/card/:cardId', (req, res) => {
-    const cardId = req.params.cardId;
-    const card = req.body;
-    cards = cards.map(el => el.id === cardId ? ( {...card, id: el.id}) : el);
-    res.send("Card updated!")
-})
+bodyParser(app);
+routes(app);
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
